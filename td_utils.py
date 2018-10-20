817fb9f879a1c840fbd2d670599d8b0bdfe94605
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2018-10-20 10:25:57 +0530

    Added support for reading wav files in TF

--- td_utils.py
+++ td_utils.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import os
 from pydub import AudioSegment
+import tensorflow as tf
+# For reading audio files
+from tensorflow.contrib.framework.python.ops import audio_ops
 
 
 # Calculate and plot spectrogram for a wav audio file
-def graph_spectrogram(wav_file):
+def graph_spectrogram(wav_file):  # TODO
     rate, data = get_wav_info(wav_file)
     nfft = 200  # Length of each window segment
     fs = 8000  # Sampling frequencies
@@ -21,19 +24,30 @@ def graph_spectrogram(wav_file):
 
 
 # Load a wav file
-def get_wav_info(wav_file):
-    rate, data = wavfile.read(wav_file)
+def get_wav_info(wav_file):  # DONE
+
+    # Old
+    # rate, data = wavfile.read(wav_file)
+    
+    # New
+    audio_binary = tf.read_file(wav_file)
+    desired_channels=2
+    wav_decoder = audio_ops.decode_wav(audio_binary, desired_channels=desired_channels)
+    
+    # wav_decoder does a normalization step. Multiplying by 2*15 undoes that
+    data = tf.cast(wav_decoder.audio * 2**15, tf.int16)
+    rate = wav_decoder.sample_rate
     return rate, data
 
 
 # Used to standardize volume of audio clip
-def match_target_amplitude(sound, target_dBFS):
+def match_target_amplitude(sound, target_dBFS):  #TODO
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
 
 # Load raw audio files for speech synthesis
-def load_raw_audio():
+def load_raw_audio():  #TODO
     activates = []
     backgrounds = []
     negatives = []
